2f537fda6ad32abdb4be15d6608c972a4e94948f
jannchie <jannchie@gmail.com>
2020-02-08 18:56:48 +0900

    USE: Simpyder

--- start_spider_task.sh
+++ start_spider_task.sh
@@ -11,3 +11,7 @@ ps -ef | grep DanmakuAggregate | grep -v grep | cut -c 9-15 | xargs kill -9
 cd danmaku_spider/ && nohup scrapy crawl DanmakuAggregate 1>log.log 2>&1 &
 ps -ef |grep py
 
+export WORD_MONGO_URL=mongodb://jannchie:141421@dds-2ze2bbbe930fe284-pub.mongodb.rds.aliyuncs.com:3717/word
+export WORD_SERVER_IP=47.94.252.158
+export WORD_SERVER_PASSWORD=Panjianqi123
+export WORD_SERVER_USERNAME=root
